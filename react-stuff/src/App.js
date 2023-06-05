e9b3d50440331a987b169d5f09f67ecb7bfce92c
Marius Skjellerud <marius.skjellerud@sikt.no>
2023-06-05 16:47:31 +0200

    added card

--- react-stuff/src/App.js
+++ react-stuff/src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
-import { getImageUrl } from "./utils";
+import Avatar from "./Avatar";
 
+// PASSING PROPS TO A COMPONENT
+
+// Child Component
 function Clouds() {
   return (
     <>
@@ -13,22 +16,8 @@ function Clouds() {
   );
 }
 
-// PASSING PROPS TO A COMPONENT
-
-// Child Component
-function Avatar(props) {
-  let person = props.person;
-  let size = props.size;
-  return (
-    <img
-      // Props, JSX curlies
-      className="avatar"
-      src={getImageUrl(person)}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
-  );
+function Card({ children }) {
+  return <div className="card">{children}</div>;
 }
 
 // Main component (Parent)
@@ -36,12 +25,24 @@ export default function Profile() {
   return (
     <div>
       {/* double curly brackets means an Object inside JSX curlies */}
-      <Avatar
-        size={100}
-        person={{ name: "Katsuko Saruhashi", imageId: "YfeOqp2" }}
-      />
-      <Avatar size={80} person={{ name: "Aklilu Lemma", imageId: "OKS67lh" }} />
-      <Avatar size={50} person={{ name: "Lin Lanying", imageId: "1bX5QH6" }} />
+      <Card>
+        <Avatar
+          size={100}
+          person={{ name: "Katsuko Saruhashi", imageId: "YfeOqp2" }}
+        />
+      </Card>
+      <Card>
+        <Avatar
+          size={80}
+          person={{ name: "Aklilu Lemma", imageId: "OKS67lh" }}
+        />
+      </Card>
+      <Card>
+        <Avatar
+          size={50}
+          person={{ name: "Lin Lanying", imageId: "1bX5QH6" }}
+        />
+      </Card>
       <Clouds />
     </div>
   );
