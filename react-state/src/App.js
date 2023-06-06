72d62112b507f01966291e0e1818f21fd4319070
Marius Skjellerud <marius.skjellerud@sikt.no>
2023-06-06 15:17:12 +0200

    added functionality and data set

--- react-state/src/App.js
+++ react-state/src/App.js
@@ -1,11 +1,33 @@
 import "./App.css";
+import {sculptureList} from "./data";
 
-function App() {
-  return (
-    <div>
-      <h1>Hola, Mundo!</h1>
-    </div>
-  );
+export default function Gallery(){
+  let index = 0;
+
+  function handleClick(){
+    index = index + 1;
+  }
+
+  let sculpture = sculptureList[index];
+  return(
+    <>
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index +1} of {sculptureList.length})
+      </h3>
+      <img
+        src={sculpture.url}
+        alt={sculpture.alt}
+      />
+      <p>
+        {sculpture.description}
+      </p>
+    </>
+  )
 }
-
-export default App;
