f260f9212061fef24db711f18e19484035ff2d31
Marius Skjellerud <marius.skjellerud@sikt.no>
2023-06-13 14:32:22 +0200

    added content

--- markup-with-jsx/src/App.js
+++ markup-with-jsx/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import "./App.css";
+import Bio from "./Bio.js";
 
-function App() {
+export default function TodoList() {
   return (
-    <div>
-      <p>Hola, Mundo!</p>
-    </div>
+    <>
+      <Bio />
+      <h1>Lamarr's Todos</h1>
+      <img style={{ border: "2px dotted red" }}
+        src="https://i.imgur.com/yXOvdOSs.jpg" 
+        alt="Lamarr" 
+        className="photo" />
+      <ul>
+        <li>Invent</li>
+        <li>Rehearse</li>
+        <li>Improve</li>
+      </ul>
+    </>
   );
 }
-
-export default App;
