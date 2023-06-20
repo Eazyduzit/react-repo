9abc79e14b133611bc7cd3bd69cbcff2a97f7765
Marius Skjellerud <marius.skjellerud@sikt.no>
2023-06-20 15:52:52 +0200

    cleaned up, added button component and some css

---
+++ my-first-next-app/src/app/counter.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function Counter() {
+  let [count, setCount] = useState(0);
+  if(count === 101){
+    setCount(count = 0);
+  }
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>
+        Click me, please im desperate!
+      </button>
+      <p>Ahh, You clicked me {count} times</p>
+    </div>
+  );
+}
