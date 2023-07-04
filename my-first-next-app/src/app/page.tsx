56dabaa47f2d2af1c40bb4ae92243a3adab0e7ff
Marius Skjellerud <marius.skjellerud@sikt.no>
2023-07-04 15:11:02 +0200

    added npm link from storybook in other local project

--- my-first-next-app/src/app/page.tsx
+++ my-first-next-app/src/app/page.tsx
@@ -1,10 +1,15 @@
+"use client"
+
 import Counter from "./counter";
+
+import {Counter as SdsCounter} from "@sikt/sds-confetti-button"; 
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-64">
       <div className="test-class">Hola, Mundo!</div>
       <Counter />
+      <SdsCounter />
     </div>
   );
 }
