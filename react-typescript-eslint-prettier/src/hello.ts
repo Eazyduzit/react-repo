812115ec4640ee0c5fb80134cf90db2ff5e81e49
Marius Skjellerud <marius.skjellerud@sikt.no>
2023-10-10 12:35:35 +0200

    annotations, promises

--- react-typescript-eslint-prettier/src/hello.ts
+++ react-typescript-eslint-prettier/src/hello.ts
@@ -5,4 +5,22 @@ function greet(person: string, date: Date) {
 }
 greet('Marius', new Date())
 
+// parameter type annotation
+function greet1(name: string) {
+  console.log('Hello, ' + name.toUpperCase() + '!!')
+}
+greet1('Marius')
+
+// return type annotations
+function getFavouriteNumber(): number {
+  return 26
+}
+getFavouriteNumber()
+
+// functions which return promises
+async function getFavouriteNumber1(): Promise<number> {
+  return 26
+}
+getFavouriteNumber1()
+
 export {}
